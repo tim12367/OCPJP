44934b343e00573dd66e62ec0fcb243f15bdf10d
Tim Chang <tim12367@ymail.com>
2023-02-02 19:44:52 +0800

    Throws exception to bondary

--- MovieLine_Lib/src/uuu/movieline/service/CustomerService.java
+++ MovieLine_Lib/src/uuu/movieline/service/CustomerService.java
@@ -1,11 +1,12 @@
 package uuu.movieline.service;
 
 import uuu.movieline.entity.Customer;
+import uuu.movieline.exception.MLException;
 
 public class CustomerService {
 	private CustomersDAO dao = new CustomersDAO();
 	
-	public Customer login(String idOrEmail, String password) {
+	public Customer login(String idOrEmail, String password) throws MLException  {
 		if(idOrEmail == null||idOrEmail.length()==0
 				||password == null||password.length() == 0) {
 			throw new IllegalArgumentException("登入時帳號密碼必須有值");
@@ -16,10 +17,10 @@ public class CustomerService {
 			return c;
 		}
 		
-		throw new RuntimeException("登入失敗!帳號或密碼不正確");
+		throw new MLException("登入失敗!帳號或密碼不正確");
 	}
 
-	public void register(Customer c){
+	public void register(Customer c) throws MLException{
 		if(c==null){
 			throw new IllegalArgumentException("註冊會員時Customer物件不得為null");
 		}
