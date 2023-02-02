9b0e82cef720510cb0263ad0e1b5062fc2bee23b
Tim Chang <tim12367@ymail.com>
2023-02-02 19:46:04 +0800

    Change RuntimeException to MLException Throw exception

--- MovieLine_Lib/src/uuu/movieline/service/MySQLConnection.java
+++ MovieLine_Lib/src/uuu/movieline/service/MySQLConnection.java
@@ -4,6 +4,8 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
+import uuu.movieline.exception.MLException;
+
 class MySQLConnection {
 	private static final String driver = "com.mysql.cj.jdbc.Driver";// mysql 8.0.12
 	private static final String url = "jdbc:mysql://localhost:3306/vgb";
@@ -12,7 +14,8 @@ class MySQLConnection {
 	static {
 		//TODO:從外部設定檔讀取
 	}
-	static Connection getConnection() {
+	//因為要回傳到別的Class 所以用static才不會執行完Connection就結束
+	static Connection getConnection() throws MLException{
 		try {
 			//1.載入JDBC Driver
 			Class.forName(driver);
@@ -22,10 +25,10 @@ class MySQLConnection {
 						DriverManager.getConnection(url, userid, pwd);
 				return connection;//回傳connection
 			} catch (SQLException e) {
-				throw new RuntimeException("建立Database連線失敗",e);
+				throw new MLException("建立Database連線失敗",e);
 			}
 		} catch (ClassNotFoundException e) {
-			throw new RuntimeException("載入JDBC Driver失敗",e);
+			throw new MLException("載入JDBC Driver失敗",e);
 		}
 	}
 }
