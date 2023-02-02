4a09e8251418c31b8d3ccea1f6fe14ca4beb779a
Tim Chang <tim12367@ymail.com>
2023-02-02 19:19:15 +0800

    commit

--- MovieLine_Lib/src/uuu/movieline/exception/MLInvalidDataException.java
+++ MovieLine_Lib/src/uuu/movieline/exception/MLInvalidDataException.java
@@ -5,7 +5,7 @@ public class MLInvalidDataException extends RuntimeException {
 	public MLInvalidDataException() {
 		super();
 	}
-
+	//兩個引數 多了拋出底層原始錯誤
 	public MLInvalidDataException(String message, Throwable cause) {
 		super(message, cause);
 	}
