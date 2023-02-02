a0db69ee40519d87877d5fa6bff2352f0cbff5df
Tim Chang <tim12367@ymail.com>
2023-02-02 18:58:30 +0800

    Add MLException.java

---
+++ MovieLine_Lib/src/uuu/movieline/exception/MLException.java
@@ -0,0 +1,20 @@
+package uuu.movieline.exception;
+
+public class MLException extends Exception {
+
+	public MLException() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public MLException(String message, Throwable cause) {
+		super(message, cause);
+		// TODO Auto-generated constructor stub
+	}
+
+	public MLException(String message) {
+		super(message);
+		// TODO Auto-generated constructor stub
+	}
+	
+}
