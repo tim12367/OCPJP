9e8d32db8046aa499476bd6f6c1ff53efb5b77df
Tim Chang <tim12367@ymail.com>
2023-02-03 09:52:04 +0800

    Edit sql

--- MovieLine_Lib/sql/INSERT_CUSTOMER_2023_01_06_0815.sql
+++ MovieLine_Lib/sql/INSERT_CUSTOMER_2023_01_06_0815.sql
@@ -4,13 +4,13 @@ LIMIT 0, 1000
 
 -- Date: 2023-02-02 20:16
 */
-INSERT INTO `` (`id`,`email`,`password`,`name`,`birthday`,`gender`,`address`,`phone`,`subscribed`) VALUES ('A123123123','test01@uuu.com','12345;lkj','李四','2000-01-01','M','','02-23456789',0);
-INSERT INTO `` (`id`,`email`,`password`,`name`,`birthday`,`gender`,`address`,`phone`,`subscribed`) VALUES ('A123123132','test20@uuu.com','12345;lkj','林梅莉','2005-08-15','M','台北市信義路二段100號','',0);
-INSERT INTO `` (`id`,`email`,`password`,`name`,`birthday`,`gender`,`address`,`phone`,`subscribed`) VALUES ('A123456752','test752@uuu.com','12345;lkj','武厄','2001-06-01','U','高雄中和區中山路4段5號','',0);
-INSERT INTO `` (`id`,`email`,`password`,`name`,`birthday`,`gender`,`address`,`phone`,`subscribed`) VALUES ('A123456761','test761@uuu.com.tw','12345;lkj','露易莎','1999-12-12','U','臺北市信義區信義路5段7號73樓之1','02-87296000',1);
-INSERT INTO `` (`id`,`email`,`password`,`name`,`birthday`,`gender`,`address`,`phone`,`subscribed`) VALUES ('A123456770','test770@uuu.com.tw','12345;lkj','碧螺春','2000-05-05','M','','',0);
-INSERT INTO `` (`id`,`email`,`password`,`name`,`birthday`,`gender`,`address`,`phone`,`subscribed`) VALUES ('A123456789','test02@uuu.com','12345;lkj','張三','2000-01-01','M','','',1);
-INSERT INTO `` (`id`,`email`,`password`,`name`,`birthday`,`gender`,`address`,`phone`,`subscribed`) VALUES ('A136304061','test03@uuu.com','12345;lkj','金凱利','2005-08-15','F','台北市復興北路99號14F','02-23456789',1);
-INSERT INTO `` (`id`,`email`,`password`,`name`,`birthday`,`gender`,`address`,`phone`,`subscribed`) VALUES ('C133258957','test04@uuu.com','12345;lkj','白帥帥','1999-09-09','M','','',0);
-INSERT INTO `` (`id`,`email`,`password`,`name`,`birthday`,`gender`,`address`,`phone`,`subscribed`) VALUES ('G177224855','test05@uuu.com','12345;lkj','湯米','2005-07-07','U','新北市中和區中山路信義路4段5號','',0);
-INSERT INTO `` (`id`,`email`,`password`,`name`,`birthday`,`gender`,`address`,`phone`,`subscribed`) VALUES ('N181149333','test@uuu.com','12345;lkj','張三豐','1996-12-12','U','','',1);
+INSERT INTO customers (id,email,password,name,birthday,gender,address,phone,subscribed) VALUES ('A123123123','test01@uuu.com','12345;lkj','李四','2000-01-01','M','','02-23456789',0);
+INSERT INTO customers (id,email,password,name,birthday,gender,address,phone,subscribed) VALUES ('A123123132','test20@uuu.com','12345;lkj','林梅莉','2005-08-15','M','台北市信義路二段100號','',0);
+INSERT INTO customers (id,email,password,name,birthday,gender,address,phone,subscribed) VALUES ('A123456752','test752@uuu.com','12345;lkj','武厄','2001-06-01','U','高雄中和區中山路4段5號','',0);
+INSERT INTO customers (id,email,password,name,birthday,gender,address,phone,subscribed) VALUES ('A123456761','test761@uuu.com.tw','12345;lkj','露易莎','1999-12-12','U','臺北市信義區信義路5段7號73樓之1','02-87296000',1);
+INSERT INTO customers (id,email,password,name,birthday,gender,address,phone,subscribed) VALUES ('A123456770','test770@uuu.com.tw','12345;lkj','碧螺春','2000-05-05','M','','',0);
+INSERT INTO customers (id,email,password,name,birthday,gender,address,phone,subscribed) VALUES ('A123456789','test02@uuu.com','12345;lkj','張三','2000-01-01','M','','',1);
+INSERT INTO customers (id,email,password,name,birthday,gender,address,phone,subscribed) VALUES ('A136304061','test03@uuu.com','12345;lkj','金凱利','2005-08-15','F','台北市復興北路99號14F','02-23456789',1);
+INSERT INTO customers (id,email,password,name,birthday,gender,address,phone,subscribed) VALUES ('C133258957','test04@uuu.com','12345;lkj','白帥帥','1999-09-09','M','','',0);
+INSERT INTO customers (id,email,password,name,birthday,gender,address,phone,subscribed) VALUES ('G177224855','test05@uuu.com','12345;lkj','湯米','2005-07-07','U','新北市中和區中山路信義路4段5號','',0);
+INSERT INTO customers (id,email,password,name,birthday,gender,address,phone,subscribed) VALUES ('N181149333','test@uuu.com','12345;lkj','張三豐','1996-12-12','U','','',1);
