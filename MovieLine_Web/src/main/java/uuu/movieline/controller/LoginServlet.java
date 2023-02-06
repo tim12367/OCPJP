966d800d1ba03085d52a463bd80c8cff17112f6f
tim12367 <tim12367@ymail.com>
2023-02-06 13:35:52 +0800

    Add error list Add error response page

--- MovieLine_Web/src/main/java/uuu/movieline/controller/LoginServlet.java
+++ MovieLine_Web/src/main/java/uuu/movieline/controller/LoginServlet.java
@@ -2,6 +2,8 @@ package uuu.movieline.controller;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -38,41 +40,74 @@ public class LoginServlet extends HttpServlet {
     @Override 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) 
 			throws ServletException, IOException {
-		//1.取得request中的form data(username,password,captcha)
+    	//錯誤清單 宣告成上層型別較靈活 之後可改
+    	List<String> errors = new ArrayList<String>();
+    	
+    	//1.取得request中的form data(username,password,captcha)
     	String username = request.getParameter("username");
     	String password = request.getParameter("password");
     	String captcha = request.getParameter("captcha");
-    	//TODO:檢查
+    	
+    	//檢查有無錯誤 可能網頁form沒給name
+    	if(username==null ||username.length()==0) {
+    		errors.add("必須輸入帳號或email");
+    	}
+    	if(password==null ||password.length()==0) {
+    		errors.add("必須輸入密碼");
+    	}
+    	if(captcha==null ||captcha.length()==0) {
+    		errors.add("必須輸入驗證碼");
+    	}
     	
     	//2.若檢查無誤，則呼叫商業邏輯
-    	CustomerService service = new CustomerService();
-    	try {
-			Customer c = service.login(username, password);
-			//3.1 產生成功的html 回應
-			response.setContentType("text/html");
-            response.setCharacterEncoding("UTF-8");
-            try (PrintWriter out = response.getWriter()) {
-                out.println("<!DOCTYPE html>");
-                out.println("<html>");
-                out.println("<head>");
-                out.println("<title>登入成功</title>");
-                out.println("</head>");
-                out.println("<body>");
-                out.println("<h1>登入成功," + c.getName() + "</h1>");
-                out.println("</body>");
-                out.println("</html>");
-            }
+    	if (errors.isEmpty()) {
+			CustomerService service = new CustomerService();
+			try {
+				Customer c = service.login(username, password);
+				//3.1 產生成功的html 回應
+				response.setContentType("text/html");
+				response.setCharacterEncoding("UTF-8");
+				try (PrintWriter out = response.getWriter()) {
+					out.println("<!DOCTYPE html>");
+					out.println("<html>");
+					out.println("<head>");
+					out.println("<title>登入成功</title>");
+					out.println("<meta http-equiv=\"refresh\" content=\"5; url=./\">");
+					out.println("</head>");
+					out.println("<body>");
+					out.println("<h1>登入成功," + c.getName() + "</h1>");
+					out.println("</body>");
+					out.println("</html>");
+				}
 
-            return;
-		} catch (LoginFailException e) {
-			this.log(e.getMessage());
-		} catch (MLException e) {
-			this.log(e.getMessage(), e);
-		}catch (Exception e) {
-			this.log("登入失敗，系統發生錯誤", e);
+				return;
+			} catch (LoginFailException e) {
+				this.log(e.getMessage());
+				errors.add(e.getMessage());//for user
+			} catch (MLException e) {
+				this.log(e.getMessage(), e);//for admin, tester, developer
+				errors.add(e.getMessage()+":"+e+", 請聯絡系統管理人員");
+			} catch (Exception e) {
+				this.log("登入失敗，系統發生非預期錯誤", e);
+				errors.add("系統發生錯誤"+e+"請聯絡系統管理人員");
+			} 
 		}
     	
-    	//3.1 產生失敗的 html
+    	//3.2 產生失敗的 html
+    	response.setContentType("text/html");
+		response.setCharacterEncoding("UTF-8");
+		try (PrintWriter out = response.getWriter()) {
+			out.println("<!DOCTYPE html>");
+			out.println("<html>");
+			out.println("<head>");
+			out.println("<title>登入失敗</title>");
+			out.println("<meta http-equiv=\"refresh\" content=\"5; url=./login.html\">");
+			out.println("</head>");
+			out.println("<body>");
+			out.println("<h1>"+ errors +", 5秒後回登入畫面"+"</h1>");
+			out.println("</body>");
+			out.println("</html>");
+		}
 	}
 
 }
