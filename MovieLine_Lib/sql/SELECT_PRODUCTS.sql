2fbcb5d6108ec1633a3b1b793abc2bb821fc2c04
tim12367 <tim12367@ymail.com>
2023-02-14 18:38:52 +0800

    sql

--- MovieLine_Lib/sql/SELECT_PRODUCTS.sql
+++ MovieLine_Lib/sql/SELECT_PRODUCTS.sql
@@ -1,2 +1,21 @@
+/*E04-a:查詢全部產品*/ 
 SELECT id, name, unit_price, stock, description, 
-photo_url, launch_date, category, discount, box_office FROM products;
+photo_url, launch_date, category, discount, box_office 
+FROM products;
+
+/*E04-b:用關鍵字查詢*/ 
+SELECT id, name, unit_price, stock, description, 
+	photo_url, launch_date, category, discount, box_office 
+FROM products WHERE name LIKE '%the%';
+
+/*E04-b:用分類查詢*/ 
+SELECT id, name, unit_price, stock, description, 
+	photo_url, launch_date, category, discount, box_office 
+FROM products WHERE category='劇情';
+
+/*E04-d:新品上架*/
+SELECT id, name, unit_price, stock, description, 
+	photo_url, launch_date, category, discount, box_office 
+FROM products
+/*WHERE launch_date <= curdate()*/
+ORDER BY launch_date DESC limit 3;
