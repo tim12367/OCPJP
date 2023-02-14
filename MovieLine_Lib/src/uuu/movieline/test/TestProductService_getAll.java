5d4122b371ab0bf0c40c392913e4b0d87eed00ee
tim12367 <tim12367@ymail.com>
2023-02-14 18:38:35 +0800

    test

---
+++ MovieLine_Lib/src/uuu/movieline/test/TestProductService_getAll.java
@@ -0,0 +1,33 @@
+package uuu.movieline.test;
+
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import uuu.movieline.entity.Product;
+import uuu.movieline.service.ProductService;
+
+public class TestProductService_getAll {
+
+	public static void main(String[] args) {
+		ProductService service =new ProductService();
+		
+//		try {
+//			List<Product> list = service.getAllProducts();
+//			System.out.println(list);
+//		} catch (Exception e) {
+//			Logger.getLogger("測試[全部產品]").log(
+//					Level.SEVERE,e.getMessage(),e);
+//		}
+		//用名子查詢
+		try {
+			List<Product> list = service.getProductsByKeyword("先生");
+			System.out.println(list);
+		} catch (Exception e) {
+			Logger.getLogger("測試[查詢產品]").log(
+					Level.SEVERE,e.getMessage(),e);
+		}
+
+	}
+
+}
