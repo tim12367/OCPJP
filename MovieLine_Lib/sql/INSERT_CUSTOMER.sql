d430b42cd7880da47ae7aa67fcd3b8b3129301e3
tim12367 <tim12367@ymail.com>
2023-02-23 12:21:31 +0800

    backup sql

---
+++ MovieLine_Lib/sql/INSERT_CUSTOMER.sql
@@ -0,0 +1,6 @@
+SELECT * FROM vgb.customers;
+UPDATE customers SET 
+ email = 'test333@uuu.com', password ='12345678', name ='張山風', 
+ birthday ='1999-10-10', gender ='U',
+ address ='新北三重區中正路', phone ='0967867678', subscribed ='0', discount ='8'
+ WHERE id ='N181149333';
