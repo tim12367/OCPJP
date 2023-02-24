c7b1bd8f33a6e4c25fdef15a55a8d672dfd0c4a2
tim12367 <tim12367@ymail.com>
2023-02-24 19:26:34 +0800

    backup sql

--- MovieLine_Lib/sql/SELECT_PRODUCT_JOIN.sql
+++ MovieLine_Lib/sql/SELECT_PRODUCT_JOIN.sql
@@ -1,6 +1,29 @@
-SELECT id, name, subtitle, unit_price, stock, 
+/*cross join*/ 
+SELECT id, name, subtitle, unit_price, products.stock, 
 	description, photo_url, trailer_url, 
     launch_date, category, discount, box_office, 
-    director, cast
+	director, cast, 
+    row_name, seats_booked, product_seats.stock 
 FROM products
 	JOIN product_seats;
+/*Inner Join*/
+SELECT id, name, subtitle, unit_price, products.stock AS total_stock, 
+	description, photo_url, trailer_url, 
+    launch_date, category, discount, box_office, 
+	director, cast 
+    row_name, seats_booked, product_seats.stock AS stock 
+FROM products INNER JOIN product_seats
+	on id=product_id
+    where id = '1'
+    order by id,showing;
+
+/* ***LEFT OUTER Join FOR E05*/
+SELECT id, name, subtitle, unit_price, products.stock , 
+	description, photo_url, trailer_url, 
+    launch_date, category, discount, box_office, 
+	director, cast, 
+    row_name, seats_booked, product_seats.stock AS row_stock ,showing 
+FROM products LEFT OUTER JOIN product_seats
+	on id=product_id
+    where id = '1'
+    order by id,showing;
