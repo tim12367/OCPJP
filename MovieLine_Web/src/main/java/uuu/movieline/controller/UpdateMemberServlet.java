2532a28763ed7ab6b2bf5450d75b99920c83e92a
tim12367 <tim12367@ymail.com>
2023-02-26 00:19:11 +0800

    Add update member Add forward to /member/update_member.jsp

--- MovieLine_Web/src/main/java/uuu/movieline/controller/UpdateMemberServlet.java
+++ MovieLine_Web/src/main/java/uuu/movieline/controller/UpdateMemberServlet.java
@@ -5,6 +5,7 @@ import java.io.PrintWriter;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -42,7 +43,9 @@ public class UpdateMemberServlet extends HttpServlet {
 		//!設定編碼一定要在request.getParameter之前
 		request.setCharacterEncoding("UTF-8");//欄位有中文必加
 		HttpSession session = request.getSession();
+		//獲取已登入的會員資料
 		Customer member = (Customer)session.getAttribute("member");
+		//若未登入回到登入畫面
 		if(member == null) {
 			response.sendRedirect("../login.jsp");
 			return;
@@ -53,6 +56,7 @@ public class UpdateMemberServlet extends HttpServlet {
 		String id = request.getParameter("id");
 		String email = request.getParameter("email");
 		String password = request.getParameter("password");
+		String newpassword = request.getParameter("newpassword");
 		String name = request.getParameter("name");
 		String birthday = request.getParameter("birthday");
 		String gender = request.getParameter("gender");
@@ -62,44 +66,43 @@ public class UpdateMemberServlet extends HttpServlet {
 		String phone = request.getParameter("phone");
 		String subscribed = request.getParameter("subscribed");
 		//檢查
+		
 		if(!member.getId().equals(id)) {
 			errors.add("不得竄改帳號!");
-		}
-		if(email == null || (email=email.trim()).length()==0) {
-			errors.add("請輸入email");
-		}
-		if(password == null || password.length()==0) {
-			errors.add("請輸入密碼");
-		}
-		if(name == null || (name=name.trim()).length()==0) {
-			errors.add("請輸入姓名");
 		}
 		if(!(member.getBirthday().toString()).equals(birthday)) {
 			errors.add("不得竄改生日!");
 		}
+		if(email == null || (email=email.trim()).length()==0) {
+			errors.add("請輸入email");
+		}
+		if(!member.getPassword().equals(password)) {
+			errors.add("舊密碼錯誤請重新輸入");
+		}
+		if(name == null || (name=name.trim()).length()==0) {
+			errors.add("請輸入姓名");
+		}
 		if(gender == null || (gender=gender.trim()).length()!=1) {
 			errors.add("請輸入性別");
-		}
-		//TODO:確定驗證碼
-		if(captcha == null || (captcha=captcha.trim()).length()==0) {
-			errors.add("請輸入驗證碼");
 		}
 		//2.若無誤，呼叫商業邏輯
 		if(errors.isEmpty()) {
 			Customer c;
 			try {
-				c = member.getClass().newInstance(); //TODO:
+				c = member.getClass().newInstance();
 			} catch (IllegalAccessException|InstantiationException e) {
 				c = new Customer();
 			}
 			try {
 				c.setId(member.getId());
 				c.setEmail(email);
-				c.setPassword(password);
+				c.setPassword(
+						newpassword==""?
+						member.getPassword():newpassword);
 				c.setName(name);
-				c.setBirthday(birthday);
+				c.setBirthday(member.getBirthday());
 				c.setGender(gender.charAt(0));//char
-				//TODO:captcha,address,phone,subscribed
+				
 				c.setAddress(address);
 				c.setPhone(phone);
 				c.setSubscribed(subscribed!=null);//boolean
@@ -111,48 +114,30 @@ public class UpdateMemberServlet extends HttpServlet {
 				member = service.login(c.getId(), c.getPassword());
 				session.setAttribute("member", member);
 				
-				response.setContentType("text/html");
-				response.setCharacterEncoding("UTF-8");
+				RequestDispatcher dispatcher = 
+						request.getRequestDispatcher("update_ok.jsp");
+				request.setAttribute("member",c);
+				request.setAttribute("msg","會員修改成功");
+				dispatcher.forward(request, response);
 				//若在getWriter前沒設定會變預設
-				try (PrintWriter out = response.getWriter()) {//defalt ISO-8859-1
-					out.println("<!DOCTYPE html>");
-					out.println("<html>");
-					out.println("<head>");
-					out.println("<title>註冊成功</title>");
-					out.println("<meta http-equiv=\"refresh\" content=\"5; url=./\">");
-					out.println("</head>");
-					out.println("<body>");
-					out.println("<h1>註冊成功," + c.getName() + ", 5秒後回登入畫面"+ "</h1>");
-					out.println("</body>");
-					out.println("</html>");
-				}
+				return;//若失敗會繼續向下
 			} catch (MLInvalidDataException ex) {
 				errors.add(ex.getMessage());//for user
 			} catch (MLException ex) {
 				errors.add(ex.getMessage()+", 請聯絡系統管理人員");//for users
 				this.log(ex.getMessage(),ex);//for admin, developer, tester
 			} catch (Exception ex) {
-				errors.add("註冊失敗，系統發生錯誤"+ex+",請聯絡系統管理人員");//for users
-				this.log("註冊失敗，系統發生非預期錯誤",ex);//for admin, developer, tester
+				errors.add("修改失敗，系統發生錯誤"+ex+",請聯絡系統管理人員");//for users
+				this.log("修改失敗，系統發生非預期錯誤",ex);//for admin, developer, tester
 			}
 		}
 		
 		
-		//3.2 顯示註冊失敗畫面
-		response.setContentType("text/html");
-		response.setCharacterEncoding("UTF-8");
-		try (PrintWriter out = response.getWriter()) {
-			out.println("<!DOCTYPE html>");
-			out.println("<html>");
-			out.println("<head>");
-			out.println("<title>註冊失敗</title>");
-			out.println("<meta http-equiv=\"refresh\" content=\"15; url=./register.jsp\">");
-			out.println("</head>");
-			out.println("<body>");
-			out.println("<h1>"+ errors +", 15秒後回登入畫面"+"</h1>");
-			out.println("</body>");
-			out.println("</html>");
-		}
+		//3.2 顯示修改失敗 返回修改
+		RequestDispatcher dispatcher = //請求派遣器
+				request.getRequestDispatcher("/member/update_member.jsp");
+		request.setAttribute("errors",errors);
+		dispatcher.forward(request, response);
 	}
 
 }
