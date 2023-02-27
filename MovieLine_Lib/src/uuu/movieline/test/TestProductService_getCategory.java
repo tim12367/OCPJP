3b1784940221ee05c73a47b781f02f7f6f8f30fe
tim12367 <tim12367@ymail.com>
2023-02-27 23:38:57 +0800

    test

---
+++ MovieLine_Lib/src/uuu/movieline/test/TestProductService_getCategory.java
@@ -0,0 +1,36 @@
+package uuu.movieline.test;
+
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import uuu.movieline.entity.Movie;
+import uuu.movieline.service.ProductService;
+
+public class TestProductService_getCategory {
+
+	public static void main(String[] args) {
+		ProductService service =new ProductService();
+		
+//		try {
+//			List<Product> list = service.getAllProducts();
+//			System.out.println(list);
+//		} catch (Exception e) {
+//			Logger.getLogger("測試[全部產品]").log(
+//					Level.SEVERE,e.getMessage(),e);
+//		}
+		//用名子查詢
+		try {
+			List<String[]> list = service.getMovieCategoryGroupByCategory();
+			for(int i =0;i<list.size();i++) {
+				System.out.println(list.get(i)[0]+list.get(i)[1]);
+			}
+			
+		} catch (Exception e) {
+			Logger.getLogger("測試[獲得分類]").log(
+					Level.SEVERE,e.getMessage(),e);
+		}
+
+	}
+
+}
