273ec4d23a528710f59885f1019477a40c551f67
tim12367 <tim12367@ymail.com>
2023-02-27 23:37:37 +0800

    backup sql

--- MovieLine_Lib/sql/SELECT_PRODUCTS.sql
+++ MovieLine_Lib/sql/SELECT_PRODUCTS.sql
@@ -1,32 +1,32 @@
 /*E04-a:查詢全部產品*/ 
 SELECT id, name, unit_price, stock, description, 
 photo_url, launch_date, category, discount, box_office 
-FROM products;
+FROM movies;
 
 /*E04-b:用關鍵字查詢*/ 
 SELECT id, name, unit_price, stock, description, 
 	photo_url, launch_date, category, discount, box_office 
-FROM products WHERE name LIKE '%the%';
+FROM movies WHERE name LIKE '%the%';
 
 /*E04-b:用分類查詢*/ 
 SELECT id, name, unit_price, stock, description, 
 	photo_url, launch_date, category, discount, box_office 
-FROM products WHERE category='劇情';
+FROM movies WHERE category='劇情';
 
 /*E04-d:新品上架*/
 SELECT id, name, unit_price, stock, description, 
 	photo_url, launch_date, category, discount, box_office 
-FROM products
+FROM movies
 /*WHERE launch_date <= curdate()*/
 ORDER BY launch_date DESC limit 3;
 
 /* 產品分類清單 */
-SELECT DISTINCT category FROM products;
+SELECT DISTINCT category FROM movies;
 
-SELECT category,COUNT(category) AS category_counter FROM products
+SELECT category,COUNT(category) AS category_counter FROM movies
 GROUP BY category;
 
 /*用id查詢*/ 
 SELECT id, name, unit_price, stock, description, 
 	photo_url, launch_date, category, discount, box_office 
-FROM products WHERE id=1;
+FROM movies WHERE id=1;
