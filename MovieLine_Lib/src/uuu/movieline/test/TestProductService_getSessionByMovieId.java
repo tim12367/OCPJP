a317a31fdf1f995837c6c67c354e9b48922f062f
tim12367 <tim12367@ymail.com>
2023-03-01 19:06:05 +0800

    test

--- MovieLine_Lib/src/uuu/movieline/test/TestProductService_getSessionByMovieId.java
+++ MovieLine_Lib/src/uuu/movieline/test/TestProductService_getSessionByMovieId.java
@@ -1,5 +1,7 @@
 package uuu.movieline.test;
 
+import java.time.LocalDate;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -23,7 +25,14 @@ public class TestProductService_getSessionByMovieId {
 		//用名子查詢
 		try {
 			List<MovieSession> list = service.getSessionsByMovieId("1");
-			System.out.println(list);
+//			System.out.println(list);
+			List<MovieSession> selectByDatelist = new ArrayList<>();
+			for(MovieSession i:list) {
+				if (i.getDate().equals(LocalDate.parse("2023-03-01"))) {
+					selectByDatelist.add(i);
+				}
+			}
+			System.out.println(selectByDatelist);
 		} catch (Exception e) {
 			Logger.getLogger("測試[id查詢]").log(
 					Level.SEVERE,e.getMessage(),e);
