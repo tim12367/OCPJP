7d24d21fd7d5a5aab859d87ec13ca3210e90951c
tim12367 <tim12367@ymail.com>
2023-03-03 11:52:13 +0800

    Remove unused import

--- MovieLine_Lib/src/uuu/movieline/entity/Movie.java
+++ MovieLine_Lib/src/uuu/movieline/entity/Movie.java
@@ -1,11 +1,6 @@
 package uuu.movieline.entity;
 
 import java.time.LocalDate;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
 import java.util.Objects;
 
 import uuu.movieline.exception.MLInvalidDataException;
