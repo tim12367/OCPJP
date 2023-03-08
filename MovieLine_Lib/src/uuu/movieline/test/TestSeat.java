95dfd26bb74e0b4d74c001bb6215955582ab4026
tim12367 <tim12367@ymail.com>
2023-03-08 16:26:38 +0800

    test

--- MovieLine_Lib/src/uuu/movieline/test/TestSeat.java
+++ MovieLine_Lib/src/uuu/movieline/test/TestSeat.java
@@ -11,10 +11,13 @@ public class TestSeat {
 		s.setRowC(0b101010);
 		s.setRowD(0b1111111111);
 		s.setRowE(0b11111);
-		s.setRowF(0b1111);
+		s.setRowF(0b0101);
 		s.setRowG(0b1111);
 		s.setRowH(0b1111);
 		s.setRowI(0b0);
+		for(String seat :s.getSeatList()) {
+			System.out.println(seat);
+		}
 	}
 
 }
