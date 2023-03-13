1641bfb366a89502eaef8d34a8aec27d60e28f9d
tim12367 <tim12367@ymail.com>
2023-03-13 17:30:08 +0800

    test

---
+++ MovieLine_Lib/src/uuu/movieline/test/TestCart.java
@@ -0,0 +1,10 @@
+package uuu.movieline.test;
+
+public class TestCart {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ MovieLine_Lib/src/uuu/movieline/test/TestOrder.java
@@ -0,0 +1,9 @@
+package uuu.movieline.test;
+
+public class TestOrder {
+
+	public static void main(String[] args) {
+		
+	}
+
+}
