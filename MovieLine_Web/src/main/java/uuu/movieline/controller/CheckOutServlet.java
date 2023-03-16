30f4d8f9f85b5b38f7776f51785efafb57cacd86
tim12367 <tim12367@ymail.com>
2023-03-16 11:04:29 +0800

    New CheckOutServlet

---
+++ MovieLine_Web/src/main/java/uuu/movieline/controller/CheckOutServlet.java
@@ -0,0 +1,32 @@
+package uuu.movieline.controller;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class CheckOutServlet
+ */
+@WebServlet("/member/check_out.do")
+public class CheckOutServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public CheckOutServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+	}
+
+}
