fecda55bf19065016968275a5ae7f10a332f47c4
tim12367 <tim12367@ymail.com>
2023-03-17 09:28:09 +0800

    Add getOrderByOrderIdAndCustomerId

--- MovieLine_Lib/src/uuu/movieline/service/OrderService.java
+++ MovieLine_Lib/src/uuu/movieline/service/OrderService.java
@@ -11,4 +11,9 @@ public class OrderService {
 	public void checkOut(Order order) throws MLException {
 		dao.insert(order);
 	}
+	public Order getOrderByOrderIdAndCustomerId(String orderId ,String customerId ) throws MLException {
+		if(orderId==null) throw new IllegalArgumentException("查詢訂單時訂單ID不可為空");
+		if(customerId==null) throw new IllegalArgumentException("查詢訂單時客戶ID不可為空");
+		return dao.selectOrderByOrderIdAndCustomerId(orderId ,customerId);
+	}
 }
